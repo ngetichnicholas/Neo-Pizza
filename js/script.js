72acaf31b7dc48e1be3ace1683f5bf73706dd501
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-26 19:43:45 +0300

    Update files

--- js/script.js
+++ js/script.js
@@ -176,3 +176,23 @@ $(document).ready(function () {
     })
   });
 });
+
+
+//Header nav Jquery
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
+
+  else {
+        $('nav').removeClass('black');
+  }
+})
